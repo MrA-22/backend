750687cdedf8faece418676b34604a09ed24e524
MrA-22 <Ariflgln@gmail.com>
2026-08-27 01:05:46 +0800

    Enable CORS middleware in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Tambahkan baris ini untuk mengaktifkan CORS secara global
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
